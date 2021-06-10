c82870f35a06ad2d1aa2e39fd37b75af1f30a3c6
Zolleander <jurgon@hot.pl>
2021-06-10 15:36:37 +0200

    ver1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { PostsComponent } from './component/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
